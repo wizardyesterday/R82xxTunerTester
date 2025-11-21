7b3f82bea50092bd7926ad25123eb49c29020944
Chris G <chris@saturn.earth>
2025-11-21 16:48:32 -0600

    I think that all needed pqrameters have been defined.

---
+++ include/R82xxTunerTester.h
@@ -0,0 +1,67 @@
+//**************************************************************************
+// file name: R82xxTunerTester.h
+//**************************************************************************
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+// This file contains all of the message defines for the R82xx tuner
+// test system.
+///_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+#ifndef __R82XXTUNERTESTER__
+
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+// Uix System V key generation support. the ftok() function uses these
+// parameters for key_t generation that is used for IPC.
+// Change these items to suit your application's needs.
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+#define KEYFILE "/home/chris/R82xxKeyFile"
+#define PROJECT_ID 'A'
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+// Message information.
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+//
+//_______________________________________________________
+// These values allow multiplexing of different message
+// types on a single queue. Each process can specify
+// *which* message type to be retrieved from the message 
+// queue. That's what I like about System V message queues.
+//_______________________________________________________
+enum MessageType
+{
+  RadioCmd = 1,
+  RadioAck,
+  SpectrumCmd,
+  SpectrumAck
+};
+//_______________________________________________________
+
+//_______________________________________________________
+// Note that, although enumerations are beinf used, the
+// opcodes are sent as the first parameter in the
+// message text. For wxample, message payload text of
+// "1 42" would imply setting of the radio IF gain of
+// 42dB. The C++ source code should make this clear.
+//_______________________________________________________
+enum RadioServerOpcode
+{
+  // Textual parameters: ifgain:Integer.
+  SendRadioCommand = 1;
+
+  // Textual parameters: None.
+  Terminate
+};
+
+enum SpectrumServerOpcode
+{
+  // Textual parameters: tag:Integer, filepath:c_string.
+  ExecuteApplicationProgram = 1,
+
+  // Textual parameters: None.
+  Terminate
+};
+//_______________________________________________________
+
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+#endif __R82XXTUNERTESTER__
