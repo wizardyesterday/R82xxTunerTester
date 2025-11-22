2b949c17340291ce32a0c96995fc9b957e34a632
Chris G <chris@saturn.earth>
2025-11-22 00:00:36 -0600

    Removed executables. Improved clarity of enumerations.

--- include/R82xxTunerTester.h
+++ include/R82xxTunerTester.h
@@ -9,7 +9,7 @@
 #ifndef __R82XXTUNERTESTER__
 
 //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
-// Uix System V key generation support. the ftok() function uses these
+// Unix System V key generation support. The ftok() function uses these
 // parameters for key_t generation that is used for IPC.
 // Change these items to suit your application's needs.
 //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
@@ -23,39 +23,51 @@
 //
 //_______________________________________________________
 // These values allow multiplexing of different message
-// types on a single queue. Each process can specify
+// types in a single queue. Each process can specify
 // *which* message type to be retrieved from the message 
-// queue. That's what I like about System V message queues.
+// queue. That's what I like about System V message
+// queues.
 //_______________________________________________________
 enum MessageType
 {
-  RadioCmd = 1,
-  RadioAck,
-  SpectrumCmd,
-  SpectrumAck
+  RadioServerCmd = 1,
+  RadioServerAck,
+  SpectrumServerCmd,
+  SpectrumServerAck
 };
 //_______________________________________________________
 
 //_______________________________________________________
-// Note that, although enumerations are being used, the
-// opcodes are sent as the first parameter in the
-// message text. For wxample, message payload text of
-// "1 42" would imply setting of the radio IF gain of
-// 42dB. The C++ source code should make this clear.
+// These enumerations are available for all servers. A
+// specific server will use enumerations that are
+// specific to that particular server. The first part 
+// of the enumeration identifies the server and the
+// rest of the enumeration describes the command
+// that is to be executed by the server. The Terminate
+// enumeration applies to all servers. It tells a
+// server to terminate execution.
+// The C++ source code should make this clearer.
+// Thus, we have a two-lwvwl hierarchy of a server
+// command: MessageType:Opcode. The message type is
+// like a routing code to a particular server, and the
+// opcode tells what the server is to do.
+// This will become clearer upon examination of the
+// opcode decoder in the source code.
 //_______________________________________________________
 enum ServerOpcodes
 {
+  // Textual parameters: None.
+  Terminate,
+
   // Textual parameters: ifgain:Integer.
-  SendRadioCommand = 1,
+  RadioServerSetIfGain,
 
   // Textual parameters: tag:Integer, filepath:c_string.
-  ExecuteApplicationProgram = 1,
-
-  // Textual parameters: None.
-  Terminate
+  SpectrumServerComputePower
 };
 //_______________________________________________________
 
 //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 
 #endif // __R82XXTUNERTESTER__
+
