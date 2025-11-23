ccb4153a6c20facaf70187a3207a40793c92e33e
Chris G <chris@saturn.earth>
2025-11-23 15:37:58 -0600

    There is now a keypressed() function that allows exiting the program when the user presses the <enter> key.

--- src/radioServer.cc
+++ src/radioServer.cc
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/select.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
@@ -9,6 +10,9 @@
 #include "TcpClient.h"
 #include "IpcMessageQueue.h"
 
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+// Local defines.
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 #define DEFAULT_SERVER_IP_ADDRESS "192.93.16.87"
 #define DEFAULT_SERVER_PORT (8001)
 
@@ -19,11 +23,143 @@ struct MyParameters
   int *serverPortPtr;
 };
 
-// Make this global.
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+// Global variables.
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 TcpClient *networkInterfacePtr;
 
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 // Static functions.
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+static bool keypressed(void);
 static void sendRadioCommand(char *commandPtr,uint32_t delayInMilliseconds);
+
+/**************************************************************************
+
+  Name: keypressed
+
+  Purpose: The purpose of this function is to determine of any key
+  was pressed on the keyboard.
+
+  Calling Sequence: status = sendRadioCommand(commandPtr,
+                                              delayInMilliseconds)
+ 
+  Inputs:
+
+    None.
+
+  Outputs:
+
+    status - An indicator of whether or not a key was pressed. A
+    value of true indicates that a key was pressed, and a value of
+    false indicates that a key was not pressed.
+
+**************************************************************************/
+static bool keypressed(void)
+{
+  bool status;
+  int stdinFileDescriptor;
+  struct timeval timeout;
+  fd_set readFds;
+
+  // Default to no key pressed.
+  status = false; 
+
+  // Retrieve the file descriptor fore stdin file stream.
+  stdinFileDescriptor = fileno(stdin);
+
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  // This block of code is necessary when working with TCP.
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  // Clear before use.
+  FD_ZERO(&readFds);
+
+  FD_SET(stdinFileDescriptor,&readFds);
+
+  // set up timeout value to 200 milliseconds.
+  timeout.tv_sec = 0;
+  timeout.tv_usec = 200000;
+
+  // wait and be nice to the system
+  select(stdinFileDescriptor+1,&readFds,0,0,&timeout);
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+  if (FD_ISSET(stdinFileDescriptor,&readFds))
+  {
+    // Indicate that a key was pressed.
+    status = true;
+  } // if
+
+  return (status);
+
+} // keypressed
+
+/**************************************************************************
+
+  Name: sendRadioCommand
+
+  Purpose: The purpose of this function is to send to send a command
+  to the radio diags application that communicates with an rtl-sdr
+  radio.
+
+  Calling Sequence: status = sendRadioCommand(commandPtr,
+                                              delayInMilliseconds)
+ 
+  Inputs:
+
+    commandPtr - A pointer to a command string that is to be sent
+    to the radio.
+
+    delayInMilliseconds - The delay for which to wait before
+    reading a response from the radio as a result of the command
+    sent. A delay value of zero will result in no delay occurring.
+
+  Outputs:
+
+    None.
+
+**************************************************************************/
+static void sendRadioCommand(char *commandPtr,uint32_t delayInMilliseconds)
+{
+  uint32_t delayInMicroseconds;
+  int octetCount;
+  char *networkBufferPtr;
+  char networkOutputBuffer[16384];
+
+  // Echo the command to the console.
+  printf("%s",commandPtr);
+
+  // Convert delay from milliseconds to microseconds.
+  delayInMicroseconds = delayInMilliseconds * 1000;
+
+  // Compute number of octets to send.
+  octetCount = strlen(commandPtr);
+
+  // Send the command to the radio.
+  octetCount = networkInterfacePtr->sendData(commandPtr,octetCount);
+
+  if (delayInMicroseconds > 0)
+  {
+    // Wait until the delay time expires.
+    usleep(delayInMicroseconds);
+  } // if
+
+  // Retrieve radio response.
+  octetCount = networkInterfacePtr->receiveData(networkOutputBuffer,16000);
+
+  
+  // Terminate the string, and output  the data to the console.
+  networkOutputBuffer[octetCount] = 0;
+  printf("%s",networkOutputBuffer);
+
+
+  return;
+
+} // sendRadioCommand
+
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+// End of static functions.
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 
 /*****************************************************************************
 
@@ -183,7 +319,6 @@ int main(int argc,char **argv)
   sendRadioCommand("start ringoscillator 8 48 0\n",0);
   sendRadioCommand("set rxfrequency 57600000\n",0);
 
-#if 0
   //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
   // Radio is set up for testing, so now message queue sruff
   // is set up.
@@ -191,7 +326,7 @@ int main(int argc,char **argv)
   // Instantiate an IPC message queue object.
   queuePtr = new IpcMessageQueue("/home/chris/R82xxKeyFile",
                                  RadioServerCmd,
-                                 false,
+                                 true, // Change to false later.
                                  &success);
 
   if (!success)
@@ -203,77 +338,21 @@ int main(int argc,char **argv)
     // Exit program.
     return (0);
   } // if
-
   //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
-#endif
+
+  // Set up for loop entry.
+  done = false;
+
+  while (!done)
+  {
+    done = keypressed();
+  } // while
 
   // Release resources.
   delete networkInterfacePtr;
-//  delete queuePtr;
+  delete queuePtr;
 
   return (0);
 
 } // main
 
-/**************************************************************************
-
-  Name: sendRadioCommand
-
-  Purpose: The purpose of this function is to send to send a command
-  to the radio diags application that communicates with an rtl-sdr
-  radio.
-
-  Calling Sequence: status = sendRadioCommand(commandPtr,
-                                              delayInMilliseconds)
- 
-  Inputs:
-
-    commandPtr - A pointer to a command string that is to be sent
-    to the radio.
-
-    delayInMilliseconds - The delay for which to wait before
-    reading a response from the radio as a result of the command
-    sent. A delay value of zero will result in no delay occurring.
-
-  Outputs:
-
-    None.
-
-**************************************************************************/
-static void sendRadioCommand(char *commandPtr,uint32_t delayInMilliseconds)
-{
-  uint32_t delayInMicroseconds;
-  int octetCount;
-  char *networkBufferPtr;
-  char networkOutputBuffer[16384];
-
-  // Echo the command to the console.
-  printf("%s",commandPtr);
-
-  // Convert delay from milliseconds to microseconds.
-  delayInMicroseconds = delayInMilliseconds * 1000;
-
-  // Compute number of octets to send.
-  octetCount = strlen(commandPtr);
-
-  // Send the command to the radio.
-  octetCount = networkInterfacePtr->sendData(commandPtr,octetCount);
-
-  if (delayInMicroseconds > 0)
-  {
-    // Wait until the delay time expires.
-    usleep(delayInMicroseconds);
-  } // if
-
-  // Retrieve radio response.
-  octetCount = networkInterfacePtr->receiveData(networkOutputBuffer,16000);
-
-  
-  // Terminate the string, and output  the data to the console.
-  networkOutputBuffer[octetCount] = 0;
-  printf("%s",networkOutputBuffer);
-
-
-  return;
-
-} // sendRadioCommand
