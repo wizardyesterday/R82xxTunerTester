8454e19a44f47a60c5e64455b4d7c01dfff0ae0a
Chris G <chris@saturn.earth>
2025-11-22 18:36:01 -0600

    The rapid prototype files have been cleaned up and commented better.

--- rapidPrototype/ipcClient.cc
+++ rapidPrototype/ipcClient.cc
@@ -3,15 +3,30 @@
 
 #include "IpcMessageQueue.h"
 
+//************************************************************
+// This program implements a client that illustrates
+// the use of System V message queues. It implements a
+// stop and wait protocol so that the server program
+// will block on its message queue waiting for a request
+// message to arrive. After this client sends a message, it
+// will block on the message queue waiting for a server
+// response message.  The result is that all transactions
+//  will be synchronized.
+//************************************************************
+
+//************************************************************
+// Mainline code.
+//************************************************************
 int main(int argc,char **argv)
 {
   bool success;
   size_t bufferLength;
   size_t sendLength;
   char buffer[1024];
-  char *messagePtr = "Client request\n";
+  char *messagePtr;
   IpcMessageQueue *queuePtr;
 
+  // Instantiate a message queue.
   queuePtr = new IpcMessageQueue("/home/chris/R82xxKeyFile",1,true,&success);
 
   if (!success)
@@ -23,11 +38,19 @@ int main(int argc,char **argv)
     return (-1);
   } // if
 
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  // This block of code sends a request message to
+  // the server.
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+   // Set the message.
+   messagePtr = "Client request\n";
+
   // Account for the string terminator.
-  sendLength = strlen(messagePtr) + 1; 
+  sendLength = strlen(messagePtr) + 1;
 
   // Send the message.
   success = queuePtr->sendData(1,messagePtr,sendLength);
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 
   if (!success)
   {
@@ -37,7 +60,11 @@ int main(int argc,char **argv)
     // Exit program.
     return (-2);
   } // if
-
+ 
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  // This block of code blocks on the message queue
+  // until a server response arrives.
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
   // Wait for response.
   success = queuePtr->receiveData(2,buffer,&bufferLength);
 
@@ -46,6 +73,7 @@ int main(int argc,char **argv)
   printf("bufferLength: %d\n",bufferLength);
   printf("message text: %s",buffer);
   } // if
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 
   // Release resources.
   delete queuePtr;
--- rapidPrototype/ipcServer.cc
+++ rapidPrototype/ipcServer.cc
@@ -7,13 +7,25 @@
 
 #include "IpcMessageQueue.h"
 
+//************************************************************
+// This program implements a server that illustrates
+// the use of System V message queues. It implements a
+// stop and wait protocol so that the client program
+// will block on its message queue after it sends a
+// request to this server. The result is that all
+// transactions will be synchronized.
+//************************************************************
+
+//************************************************************
+// Mainline code.
+//************************************************************
 int main(int argc,char **argv)
 {
   bool success;
   size_t bufferLength;
   size_t sendLength;
   char buffer[1024];
-  char *messagePtr = "Server response\n";
+  char *messagePtr;
   IpcMessageQueue *queuePtr;
 
   // Instantiate an IPC message queue object.
@@ -28,6 +40,11 @@ int main(int argc,char **argv)
     return (-1);
   } // if
 
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  // This block of code blocks on the message queue
+  // until a client request arrives.
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
   // Wait for request message.
   success = queuePtr->receiveData(1,buffer,&bufferLength);
 
@@ -35,11 +52,24 @@ int main(int argc,char **argv)
   {
     printf("bufferLength: %d\n",bufferLength);
     printf("message text: %s",buffer);
+  } // if
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+  if (success)
+  {
+    //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+    // This block of code sends a response message to
+    // the client.
+    //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+    // Set the message.
+    messagePtr = "Server Response\n";
+
     // Account for the string terminator.
     sendLength = strlen(messagePtr) + 1; 
 
     // Send the message.
     success = queuePtr->sendData(2,messagePtr,sendLength);
+    //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
   } // if
 
   // Release resources.
